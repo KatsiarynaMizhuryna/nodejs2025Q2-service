a1a4f06a3d27508f8c576c10f6300b687fd20fb5
katsiaryna <katsiaryna.mizhuryna@gmail.com>
2025-11-27 19:03:36 +0200

    create albums module

---
+++ src/albums/album.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { validate as uuidValidate } from 'uuid';
+import { AlbumService } from './album.service';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+
+@Controller('album')
+export class AlbumController {
+  constructor(private service: AlbumService) {}
+
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    if (!uuidValidate(id)) throw new BadRequestException('Invalid id');
+
+    const album = this.service.findOne(id);
+    if (!album) throw new NotFoundException();
+    return album;
+  }
+
+  @Post()
+  create(@Body() dto: CreateAlbumDto) {
+    return this.service.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateAlbumDto) {
+    if (!uuidValidate(id)) throw new BadRequestException('Invalid id');
+
+    const updated = this.service.update(id, dto);
+    if (!updated) throw new NotFoundException();
+
+    return updated;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    if (!uuidValidate(id)) throw new BadRequestException('Invalid id');
+
+    const ok = this.service.delete(id);
+    if (!ok) throw new NotFoundException();
+
+    return;
+  }
+}
---
+++ src/albums/album.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { Album } from './album.entity';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class AlbumService {
+  private albums: Album[] = [];
+
+  create(dto: CreateAlbumDto) {
+    const album: Album = {
+      id: randomUUID(),
+      name: dto.name,
+      year: dto.year,
+      artistId: dto.artistId ?? null,
+    };
+    this.albums.push(album);
+    return album;
+  }
+
+  findAll() {
+    return this.albums;
+  }
+
+  findOne(id: string) {
+    return this.albums.find((a) => a.id === id) || null;
+  }
+
+  update(id: string, dto: UpdateAlbumDto) {
+    const album = this.albums.find((a) => a.id === id);
+    if (!album) return null;
+
+    album.name = dto.name;
+    album.year = dto.year;
+    album.artistId = dto.artistId ?? null;
+
+    return album;
+  }
+
+  delete(id: string) {
+    const index = this.albums.findIndex((a) => a.id === id);
+    if (index === -1) return false;
+
+    this.albums.splice(index, 1);
+    return true;
+  }
+}
