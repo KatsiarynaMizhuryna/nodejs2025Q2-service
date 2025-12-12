34d6f07679749ccc0d317a8f06e4892f9a4af91e
katsiaryna <katsiaryna.mizhuryna@gmail.com>
2025-12-12 13:19:24 +0200

    add new modules for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingService } from './logging/logging.service';
+import { LoggingInterceptor } from './logging/logging.interseptor';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
@@ -7,9 +10,12 @@ import { AlbumModule } from './albums/album.module';
 import { TrackModule } from './tracks/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Module({
   imports: [
+    AuthModule,
     UserModule,
     ArtistModule,
     AlbumModule,
@@ -18,6 +24,17 @@ import { PrismaModule } from './prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    LoggingService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
